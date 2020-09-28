ec520689b98d54c20b27959645067b20d31e7ba0
n-ndini7 <hp@RJs-PC>
2020-09-28 12:16:57 +0530

    [R.Nandini] added uc9 - save the total wage for each company

--- src/com/capgemini/EmployeeWageBuilder.java
+++ src/com/capgemini/EmployeeWageBuilder.java
@@ -5,8 +5,21 @@ public class EmployeeWageBuilder {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 
-	// UC8 - Compute employee wage using class methods for different companies.
-	public static int EmpWageCalculation(String company, int emprateperhr, int noofworkdays, int maxhrspermonth) {
+	private final String company;
+	private final int emprateperhr;
+	private final int noofworkdays;
+	private final int maxhrspermonth;
+	private int totalwage;
+
+	// UC9 - compute employee wage and save total wage by company.
+	public EmployeeWageBuilder(String company, int emprateperhr, int noofworkdays, int maxhrspermonth) {
+		this.company = company;
+		this.emprateperhr = emprateperhr;
+		this.noofworkdays = noofworkdays;
+		this.maxhrspermonth = maxhrspermonth;
+	}
+
+	public void EmpWageCalculation() {
 		int emphrs = 0;
 		int totalhrs = 0;
 		int totalworkdays = 0;
@@ -27,13 +40,22 @@ public class EmployeeWageBuilder {
 			totalhrs += emphrs;
 			System.out.println("Day: " + totalworkdays + " emp hrs : " + emphrs);
 		}
-		int totalwage = totalhrs * emprateperhr;
-		return totalwage;
+		totalwage = totalhrs * emprateperhr;
+
+	}
+
+	@Override
+	public String toString() {
+		return "Total wage as calculated for " + company + " employee is : " + totalwage;
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Total wage as calculated for Boat employee is : " + EmpWageCalculation("Boat", 20, 3, 6));
-		System.out.println("Total wage as calculated for Reliance employee is : " + EmpWageCalculation("JBL", 30, 2, 10));
+		EmployeeWageBuilder Boat = new EmployeeWageBuilder("Boat", 20, 3, 9);
+		EmployeeWageBuilder JBL = new EmployeeWageBuilder("JBL", 20, 3, 9);
+		Boat.EmpWageCalculation();
+		System.out.println(Boat);
+		JBL.EmpWageCalculation();
+		System.out.println(JBL);
 		System.out.println("Computing employee wage using class methods for different companies!!");
 	}
 
