290e98a70e3dc149490d69bc34b373899f289faa
n-ndini7 <hp@RJs-PC>
2020-09-28 11:57:09 +0530

    [R.Nandini] added uc1 - employee presence check

--- src/com/capgemini/EmployeeWageBuilder.java
+++ src/com/capgemini/EmployeeWageBuilder.java
@@ -1,11 +1,20 @@
 package com.capgemini;
 
-public class EmployeeWageBuilder {
-
-	//Master branch - welcome message!
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Builder!!!");
+	public class EmployeeWageBuilder {
+		//UC1 - /employee presence check
+		public static void main(String[] args) {
+			int IS_FULL_TIME=1;
+			System.out.println("Employee presence check ");
+			double empcheck = Math.floor(Math.random()*10)%2;
+			if(empcheck==IS_FULL_TIME) {
+				System.out.println("Employee is present");
+			}
+			else
+			{
+				System.out.println("Employee is absent");
+			}
+			
+		}
 	}
 
-}
 
