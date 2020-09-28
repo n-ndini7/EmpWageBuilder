467a9c00448a7aa3ffb39c0a4e6bc96faf3f45b0
n-ndini7 <hp@RJs-PC>
2020-09-28 11:55:40 +0530

    [R.Nandini] added master - welcome message

---
+++ src/com/capgemini/EmployeeWageBuilder.java
@@ -0,0 +1,11 @@
+package com.capgemini;
+
+public class EmployeeWageBuilder {
+
+	//Master branch - welcome message!
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Builder!!!");
+	}
+
+}
+
