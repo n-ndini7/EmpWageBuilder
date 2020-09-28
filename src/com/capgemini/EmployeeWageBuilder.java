fab5bef1ae6395a09701d287cd084603f785e08f
n-ndini7 <hp@RJs-PC>
2020-09-28 12:05:51 +0530

    [R.Nandini] added uc3 - add part time employee and wage

--- src/com/capgemini/EmployeeWageBuilder.java
+++ src/com/capgemini/EmployeeWageBuilder.java
@@ -2,15 +2,17 @@ package com.capgemini;
 
 public class EmployeeWageBuilder {
 
-	//UC2- employee daily wage calculate
 	public static void main(String[] args) {
-		System.out.println("Employee daily wage calculate");
-		int IS_FULL_TIME = 1;
+		//UC3 - add part time employee and wage.
+		int IS_PART_TIME=1;
+		int IS_FULL_TIME = 2;
 		int EMP_RATE_PER_HR = 20;
 		int emphrs = 0;
 		int empwage = 0;
-		double empcheck = Math.floor(Math.random() * 10) % 2;
-		if (empcheck == IS_FULL_TIME)
+		double empcheck = Math.floor(Math.random() * 10) % 3;
+		if (empcheck ==IS_PART_TIME) 
+			emphrs = 4;
+		else if (empcheck == IS_FULL_TIME)
 			emphrs = 8;
 		else
 			emphrs = 0;
@@ -21,3 +23,4 @@ public class EmployeeWageBuilder {
 	}
 
 }
+
