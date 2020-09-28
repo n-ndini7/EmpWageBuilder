a183a1a3422ee7062cf2ccc1b1e395644a0f2de3
n-ndini7 <hp@RJs-PC>
2020-09-28 11:58:47 +0530

    [R.Nandini] added uc2 - employee daily wage calculation

--- src/com/capgemini/EmployeeWageBuilder.java
+++ src/com/capgemini/EmployeeWageBuilder.java
@@ -1,20 +1,23 @@
 package com.capgemini;
 
-	public class EmployeeWageBuilder {
-		//UC1 - /employee presence check
-		public static void main(String[] args) {
-			int IS_FULL_TIME=1;
-			System.out.println("Employee presence check ");
-			double empcheck = Math.floor(Math.random()*10)%2;
-			if(empcheck==IS_FULL_TIME) {
-				System.out.println("Employee is present");
-			}
-			else
-			{
-				System.out.println("Employee is absent");
-			}
-			
-		}
+public class EmployeeWageBuilder {
+
+	//UC2- employee daily wage calculate
+	public static void main(String[] args) {
+		System.out.println("Employee daily wage calculate");
+		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HR = 20;
+		int emphrs = 0;
+		int empwage = 0;
+		double empcheck = Math.floor(Math.random() * 10) % 2;
+		if (empcheck == IS_FULL_TIME)
+			emphrs = 8;
+		else
+			emphrs = 0;
+		empwage = emphrs * EMP_RATE_PER_HR;
+
+		System.out.println("Employee wage :" + empwage);
+
 	}
 
-
+}
