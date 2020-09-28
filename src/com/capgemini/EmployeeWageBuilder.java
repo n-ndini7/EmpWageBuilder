55c7e4682c22c132169d3fadb351dd54cc8354ae
n-ndini7 <hp@RJs-PC>
2020-09-28 12:08:59 +0530

    [R.Nandini] added Uc5 - compute wages for a month

--- src/com/capgemini/EmployeeWageBuilder.java
+++ src/com/capgemini/EmployeeWageBuilder.java
@@ -1,30 +1,36 @@
 package com.capgemini;
 
 public class EmployeeWageBuilder {
-	//UC4 - solving using switch case
-	public static final int IS_PART_TIME= 1;
+
+	//UC5- CALCULATING WAGES FOR A MONTH
+	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HR = 20;
+	public static final int NO_OF_WORK_DAYS = 2;
 
 	public static void main(String[] args) {
 		int emphrs = 0;
 		int empwage = 0;
-		int empcheck = (int) Math.floor(Math.random() * 10) % 3;
-		switch (empcheck) {
-		case IS_PART_TIME:
-			emphrs = 4;
-			break;
-		case IS_FULL_TIME:
-			emphrs = 8;
-			break;
-		default:
-			emphrs = 0;
-			break;
+		int totalwage = 0;
+		System.out.println("Calculating wages for a month");
+		for (int d = 0; d < NO_OF_WORK_DAYS; d++) {
+			int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empcheck) {
+			case IS_PART_TIME:
+				emphrs = 4;
+				break;
+			case IS_FULL_TIME:
+				emphrs = 8;
+				break;
+			default:
+				emphrs = 0;
+				break;
+			}
+			empwage = emphrs * EMP_RATE_PER_HR;
+			totalwage += empwage;
+			System.out.println("employee wage as calculated is: " + empwage);
 		}
-		empwage = emphrs * EMP_RATE_PER_HR;
-		System.out.println("employee wage is calculated as: " + empwage);
+		System.out.println("total emp wage as calculated is: " + totalwage);
 	}
 
 }
-
-
